0ac27510125b5cf5646f8fb4dcfe713fdb562650
Arta Razavi <artarazavi@gmail.com>
2020-02-12 15:14:40 -0800

    Updates

---
+++ precache-manifest.7eb2bca27c26b86b4dd4d281cdab0caf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543de03330e616db435d38f939df564d",
+    "url": "arta.monster/index.html"
+  },
+  {
+    "revision": "a54b951a6e544481daa3",
+    "url": "arta.monster/static/css/2.a54d8a59.chunk.css"
+  },
+  {
+    "revision": "216d79c17e3f2188e757",
+    "url": "arta.monster/static/css/main.a6079327.chunk.css"
+  },
+  {
+    "revision": "a54b951a6e544481daa3",
+    "url": "arta.monster/static/js/2.52ef88bb.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "arta.monster/static/js/2.52ef88bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216d79c17e3f2188e757",
+    "url": "arta.monster/static/js/main.0befb7a0.chunk.js"
+  },
+  {
+    "revision": "f4e88e045320befb88b6",
+    "url": "arta.monster/static/js/runtime-main.c6ece7eb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "arta.monster/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "arta.monster/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "arta.monster/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "arta.monster/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "arta.monster/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vim-arta/precache-manifest.b37fff3b90c5ff9c360a1dd26fc09896.js"
+  "arta.monster/precache-manifest.7eb2bca27c26b86b4dd4d281cdab0caf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vim-arta/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("arta.monster/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
