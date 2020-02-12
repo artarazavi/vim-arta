2e6d7b1c9d21593e292c6eb958378f35d1dd4f71
Arta Razavi <artarazavi@gmail.com>
2020-02-12 13:19:59 -0800

    Updates

---
+++ precache-manifest.5ca6d19bf41be2fe2f191480381c2bf5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244105c877ce361a3e04c303457760d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27bf7d1c29a3fb46a182",
+    "url": "/static/css/2.61e2abd8.chunk.css"
+  },
+  {
+    "revision": "ca62cdc783e7471368d1",
+    "url": "/static/css/main.a6079327.chunk.css"
+  },
+  {
+    "revision": "27bf7d1c29a3fb46a182",
+    "url": "/static/js/2.61010755.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.61010755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca62cdc783e7471368d1",
+    "url": "/static/js/main.02caaa15.chunk.js"
+  },
+  {
+    "revision": "757af73b7fbe61ce9240",
+    "url": "/static/js/runtime-main.4cf96c6b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
